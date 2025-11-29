9ba79c00eb41d47ef72ab4afcdae6f90a69232c2
karthik1Srigadha <srigadha.karthik@gmail.com>
2025-11-29 13:17:05 +0530

    update changes

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  baseURL: "https://clothing-ecommerce-k9ox.onrender.com/api",
   withCredentials: true,
 });
 
